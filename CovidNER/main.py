7e1e8a848bdf0eb09765f33c3687865bce63ed91
soori.mess <soroushmesgari2000@gmail.com>
2020-10-11 18:10:51 +0330

    hover feature added

--- CovidNER/main.py
+++ CovidNER/main.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from .views import *
 from CovidNER.ner.Text import Text
 
-app = Flask(__name__)
-
 # activate if we need DB
 # app.config.from py-file('config.py')
 # db = SQLAlchemy(app)
+
+app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
